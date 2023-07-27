a50ffe96600d73150bc8dc5a0337ec573acd5842
sujatakv <sujatakv@yahoo.com>
2023-07-27 21:40:21 +0530

    Paper trade LTP issue fixed for kite in algo

--- algo.js
+++ algo.js
@@ -1059,7 +1059,7 @@ client_api = function () {
                     }
                 })
             } else {
-                orderbook.place_paper_trade(params, live_data[params['token']])
+                orderbook.place_paper_trade(params, broker.get_ticker(params))
             }
         },
 
